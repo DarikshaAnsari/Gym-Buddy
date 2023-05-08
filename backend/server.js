0607f21e371ed5b988ef33027cc67e31d5c61448
DarikshaAnsari <100822529+DarikshaAnsari@users.noreply.github.com>
2023-05-08 23:32:26 +0530

    installing cors

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express=require('express')
 const mongoose=require('mongoose');
 const app=express()
-
+const cors = require("cors");
+app.use(cors());
 // disable CORS
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
